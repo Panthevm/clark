37c187129caa1516b638a68c73f29623ed7c83b4
Panthevm <panthevm@yandex.ru>
2019-11-30 13:47:46 +0300

    Deps update

--- ui/src/app/dev.cljs
+++ ui/src/app/dev.cljs
@@ -1,9 +1,7 @@
 (ns ^:figwheel-no-load app.dev
   (:require [app.core :as core]
-            [re-frisk.core :refer [enable-re-frisk!]]
-            [devtools.core :as devtools]))
+            [re-frisk.core :refer [enable-re-frisk!]]))
 
-(devtools/install!)
 (enable-console-print!)
 (enable-re-frisk! {:width "400px" :height "500px"})
 (core/mount-root)
